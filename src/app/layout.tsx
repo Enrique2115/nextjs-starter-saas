2ad9fbdf17f8f232a01bdb4377223a9977ff17df
Luis Enrique <lmorochofebres@gmail.com>
2025-03-05 20:35:16 -0500

    chore: add conditional script tag for development environment in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* {process.env.NODE_ENV === 'development' && (
+          <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+        )} */}
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
